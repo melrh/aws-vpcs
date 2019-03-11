624b546ee0ce01b329aeb2d0d3c765e357dbed66
melrobynhogg <melrobynhogg@btinternet.com>
2019-03-11 16:45:28 +0000

    Creates most recent ubuntu instance

--- terraform/vpc-ec2/main.tf
+++ terraform/vpc-ec2/main.tf
@@ -87,3 +87,33 @@ resource "aws_route_table" "private-rt" {
         Name    =   "priv-rt"
     }
 }
+
+/*
+    Gets latest Ubuntu AMI from specific owner
+*/
+data "aws_ami" "ubuntu" {
+    most_recent =   true
+
+    filter {
+        name    =   "name"
+        values  =   ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+    }
+
+    filter {
+        name    =   "virtualization-type"
+        values  =   ["hvm"]
+    }
+
+    owners  =   ["099720109477"]
+}
+
+resource "aws_instance" "ec2" {
+    subnet_id   =   "${aws_subnet.public.*.id[0]}"
+
+    ami             =   "${data.aws_ami.ubuntu.id}"
+    instance_type   =   "t2.micro"
+
+    tags {
+        Name        =   "ubuntu-ec2"
+    }
+}
