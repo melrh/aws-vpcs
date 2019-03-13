f7ceb56176d2639a085d6011bd12d16793d7e35b
melrobynhogg <melrobynhogg@btinternet.com>
2019-03-13 17:47:09 +0000

    Fixed build issues and finished core module

--- terraform/vpc-io/core/main.tf
+++ terraform/vpc-io/core/main.tf
@@ -2,27 +2,27 @@ resource "aws_vpc" "main" {
     cidr_block  =   "${var.vpc_cidr}"
 
     tags {
-        name    =   "main-vpc"
+        Name    =   "main-vpc"
     }
 }
 
 resource "aws_subnet" "public" {
     count       =   "${var.subnet_count}"
     vpc_id      =   "${aws_vpc.main.id}"
-    cidr_block  =   "${cidrsubnet(aws_vpc.main.id, 8, count.index)}"
+    cidr_block  =   "${cidrsubnet(var.vpc_cidr, 8, count.index)}"
 
     tags {
-        name    =   "pubsub-${count.index + 1}"
+        Name    =   "pubsub-${count.index + 1}"
     }
 }
 
 resource "aws_subnet" "private" {
     count       =   "${var.subnet_count}"
     vpc_id      =   "${aws_vpc.main.id}"
-    cidr_block  =   "${cidrsubnet(aws_vpc.main.id, 8, count.index + var.subnet_count)}"
+    cidr_block  =   "${cidrsubnet(var.vpc_cidr, 8, count.index + var.subnet_count)}"
 
     tags {
-        name    =   "privsub-${count.index + 1}"
+        Name    =   "privsub-${count.index + 1}"
     }
 }
 
@@ -30,7 +30,7 @@ resource "aws_internet_gateway" "ig" {
     vpc_id  =   "${aws_vpc.main.id}"
 
     tags {
-        name    =   "main-ig"
+        Name    =   "main-ig"
     }
 }
 
@@ -43,7 +43,7 @@ resource "aws_default_route_table" "default-route" {
     }
 
     tags {
-        name    =   "internet-route"
+        Name    =   "internet-route"
     }
 }
 
@@ -51,4 +51,34 @@ resource "aws_route_table_association" "ig-assoc" {
     count           =   "${var.subnet_count}"
     subnet_id       =   "${element(aws_subnet.public.*.id, count.index)}"
     route_table_id  =   "${aws_default_route_table.default-route.id}"
+}
+
+resource "aws_eip" "nat-eip" {
+    vpc =   true
+
+    tags {
+        Name    =   "nat-eip"
+    }
+}
+
+resource "aws_nat_gateway" "nat" {
+    subnet_id       =   "${aws_subnet.private.*.id[0]}"
+    allocation_id   =  "${aws_eip.nat-eip.id}"
+
+    tags {
+        Name    =   "nat-gw"
+    }
+}
+
+resource "aws_route_table" "nat-route" {
+    vpc_id  =   "${aws_vpc.main.id}"
+
+    route {
+        cidr_block  =   "0.0.0.0/0"
+        gateway_id  =   "${aws_nat_gateway.nat.id}"
+    }
+
+    tags {
+        Name    =   "nat-route"
+    }
 }
