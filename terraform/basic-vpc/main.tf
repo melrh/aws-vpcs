e80b4d13b5bdb34fc774da619ba85a767cfb2150
Melissa Hogg <melissarobynhogg@gmail.com>
2020-08-18 16:28:58 -0700

    Refactoring to version 0.12

--- terraform/basic-vpc/main.tf
+++ terraform/basic-vpc/main.tf
@@ -1,89 +1,96 @@
 provider aws {
-    profile =   "${var.aws_profile}"
-    region  =   "${var.aws_region}"
+  region = var.aws_region
 }
 
 resource "aws_vpc" "main" {
-    cidr_block  =   "${var.vpc_cidr}"
+  cidr_block = var.vpc_cidr
 
-    tags {
-        Name    =   "Main"
-    }
+  tags = {
+    Name    = "Main_VPC"
+    Project = "Terraform_Lab"
+  }
 }
 
 resource "aws_subnet" "public" {
-    count       =   "${var.subnet_count}"
-    vpc_id      =   "${aws_vpc.main.id}"
-    cidr_block  =   "${cidrsubnet(var.vpc_cidr, 8, count.index)}"
+  count      = var.subnet_count
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index)
 
-    tags {
-        Name    =   "pubsub-${count.index}"
-    }
+  tags = {
+    Name    = "PubSub${count.index + 1}"
+    Project = "Terraform_Lab"
+  }
 }
 
 resource "aws_subnet" "private" {
-    count       =   "${var.subnet_count}"
-    vpc_id      =   "${aws_vpc.main.id}"
-    cidr_block  =   "${cidrsubnet(var.vpc_cidr, 8, count.index + var.subnet_count)}"
+  count      = var.subnet_count
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index + var.subnet_count)
 
-    tags {
-        Name    =   "privsub-${count.index}"
-    }
+  tags = {
+    Name    = "PrivSub${count.index + 1}"
+    Project = "Terraform_Lab"
+  }
 }
 
 resource "aws_internet_gateway" "ig" {
-    vpc_id      =   "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
-    tags {
-        Name    =   "main-ig"
-    }
+  tags = {
+    Name    = "Main-IG"
+    Project = "Terraform_Lab"
+  }
 }
 
-resource "aws_default_route_table" "default-route" {
-    default_route_table_id      =   "${aws_vpc.main.default_route_table_id}"
+resource "aws_default_route_table" "default_route" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
 
-    route {
-        cidr_block  =   "0.0.0.0/0"
-        gateway_id  =   "${aws_internet_gateway.ig.id}"
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
 
-    tags {
-        Name    =   "default-route"
-    }
+  tags = {
+    Name    = "Default_Route"
+    Project = "Terraform_Lab"
+  }
 }
 
-resource "aws_route_table_association" "route-association" {
-    count           =   "${var.subnet_count}"
-    subnet_id       =   "${element(aws_subnet.public.*.id, count.index)}"
-    route_table_id  =   "${aws_default_route_table.default-route.id}"
+resource "aws_route_table_association" "route_association" {
+  count          = var.subnet_count
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_default_route_table.default_route.id
 }
 
-resource "aws_eip" "nat-eip" {
-    vpc     =   true
+resource "aws_eip" "nat_eip" {
+  vpc = true
 
-    tags {
-        Name    =   "nat-eip"
-    }
+  tags = {
+    Name    = "NAT_EIP"
+    Project = "Terraform_Lab"
+  }
 }
 
-resource "aws_nat_gateway" "nat-gw" {
-    allocation_id   =   "${aws_eip.nat-eip.id}"
-    subnet_id       =   "${aws_subnet.private.*.id[0]}"
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.private.*.id[0]
 
-    tags {
-        Name    =   "nat-gw"
-    }
+  tags = {
+    Name    = "NAT_GW"
+    Project = "Terraform_Lab"
+  }
 }
 
-resource "aws_route_table" "private-route" {
-    vpc_id  =   "${aws_vpc.main.id}"
+resource "aws_route_table" "private_route" {
+  vpc_id = aws_vpc.main.id
 
-    route {
-        cidr_block  =   "0.0.0.0/0"
-        nat_gateway_id  =   "${aws_nat_gateway.nat-gw.id}"
-    }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
 
-    tags {
-        Name    =   "priv-route"
-    }
+  tags = {
+    Name    = "Priv_Route"
+    Project = "Terraform_Lab"
+  }
 }
--- terraform/basic-vpc/variables.tf
+++ terraform/basic-vpc/variables.tf
@@ -1,7 +1,3 @@
-variable aws_profile {
-  description = "IAM profile used to run terraform"
-}
-
 variable aws_region {
   description = "AWS region the terraform will be applied to"
 }
@@ -11,5 +7,5 @@ variable vpc_cidr {
 }
 
 variable subnet_count {
-  description  =  "Amount created for each private/public subnet"
+  description = "Amount of subnets to be created for each private/public"
 }
