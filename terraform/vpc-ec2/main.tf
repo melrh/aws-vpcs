8ab0bb1956f1eb0083e9b452ed9a47571c5de7a9
Melissa Hogg <melissarobynhogg@gmail.com>
2020-08-20 09:51:25 -0700

    Refactored to version 0.12

--- terraform/vpc-ec2/main.tf
+++ terraform/vpc-ec2/main.tf
@@ -1,119 +1,127 @@
 provider "aws" {
-    profile =   "${var.aws_profile}"
-    region  =   "${var.aws_region}"
+  region = var.aws_region
 }
 
 resource "aws_vpc" "main" {
-    cidr_block  =   "${var.vpc_cidr}"
+  cidr_block = var.vpc_cidr
 
-    tags    {
-        Name    =   "main-vpc"
-    }
+  tags = {
+    Name    = "Main_VPC"
+    Project = "Terraform_Lab"
+  }
 }
 
 resource "aws_subnet" "public" {
-    vpc_id      =   "${aws_vpc.main.id}"
-    count       =   "${var.subnet_count}"
-    cidr_block  =   "${cidrsubnet(var.vpc_cidr, 8, count.index)}"
+  vpc_id     = aws_vpc.main.id
+  count      = var.subnet_count
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index)
 
-    tags    {
-        Name    =   "pubsub-${count.index + 1}"
-    }
+  tags = {
+    Name    = "PubSub${count.index + 1}"
+    Project = "Terraform_Lab"
+  }
 }
 
 resource "aws_subnet" "private" {
-    vpc_id      =   "${aws_vpc.main.id}"
-    count       =   "${var.subnet_count}"
-    cidr_block  =   "${cidrsubnet(var.vpc_cidr, 8, count.index + var.subnet_count)}"
+  vpc_id     = aws_vpc.main.id
+  count      = var.subnet_count
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, count.index + var.subnet_count)
 
-    tags {
-        Name    =   "privsub-${count.index + 1}"
-    }
+  tags = {
+    Name    = "PrivSub${count.index + 1}"
+    Project = "Terraform_Lab"
+  }
 }
 
 resource "aws_internet_gateway" "ig" {
-    vpc_id      =   "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
 
-    tags {
-        Name    =   "main-ig"
-    }
+  tags = {
+    Name    = "Main_IG"
+    Project = "Terraform_Lab"
+  }
 }
 
-resource "aws_default_route_table" "public-rt" {
-    default_route_table_id  =   "${aws_vpc.main.default_route_table_id}"
+resource "aws_default_route_table" "public_rt" {
+  default_route_table_id = aws_vpc.main.default_route_table_id
 
-    route {
-        cidr_block          =   "0.0.0.0/0"
-        gateway_id          =   "${aws_internet_gateway.ig.id}"
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ig.id
+  }
 
-    tags {
-        Name    =   "ig_route"
-    }
+  tags = {
+    Name    = "IG_Route"
+    Project = "Terraform_Lab"
+  }
 }
 
-resource "aws_route_table_association" "public-rt-assoc" {
-    count           =   "${var.subnet_count}"
-    subnet_id       =   "${element(aws_subnet.public.*.id, count.index)}"
-    route_table_id  =   "${aws_default_route_table.public-rt.id}"
+resource "aws_route_table_association" "public_rt_assoc" {
+  count          = var.subnet_count
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_default_route_table.public_rt.id
 }
 
-resource "aws_eip" "nat-eip" {
-    vpc     =   true
+resource "aws_eip" "nat_eip" {
+  vpc = true
 
-    tags {
-        Name    =   "nat-eip"
-    }
+  tags = {
+    Name    = "NAT_EIP"
+    Project = "Terraform_Lab"
+  }
 }
 
-resource "aws_nat_gateway" "nat-gw" {
-    subnet_id   =   "${aws_subnet.private.*.id[0]}"
-    allocation_id   =   "${aws_eip.nat-eip.id}"
+resource "aws_nat_gateway" "nat_gw" {
+  subnet_id     = aws_subnet.private.*.id[0]
+  allocation_id = aws_eip.nat_eip.id
 
-    tags {
-        Name    =   "main-nat"
-    }
+  tags = {
+    Name    = "Main_NAT"
+    Project = "Terraform_Lab"
+  }
 }
 
-resource "aws_route_table" "private-rt" {
-    vpc_id  =   "${aws_vpc.main.id}"
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
 
-    route {
-        cidr_block  =   "0.0.0.0/0"
-        nat_gateway_id  =   "${aws_nat_gateway.nat-gw.id}"
-    }
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
 
-    tags {
-        Name    =   "priv-rt"
-    }
+  tags = {
+    Name    = "Priv_RT"
+    Project = "Terraform_Lab"
+  }
 }
 
 /*
     Gets latest Ubuntu AMI from specific owner
 */
 data "aws_ami" "ubuntu" {
-    most_recent =   true
+  most_recent = true
 
-    filter {
-        name    =   "name"
-        values  =   ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-    }
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
 
-    filter {
-        name    =   "virtualization-type"
-        values  =   ["hvm"]
-    }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-    owners  =   ["099720109477"]
+  owners = ["099720109477"]
 }
 
 resource "aws_instance" "ec2" {
-    subnet_id   =   "${aws_subnet.public.*.id[0]}"
+  subnet_id = aws_subnet.public.*.id[0]
 
-    ami             =   "${data.aws_ami.ubuntu.id}"
-    instance_type   =   "t2.micro"
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
 
-    tags {
-        Name        =   "ubuntu-ec2"
-    }
+  tags = {
+    Name    = "Ubuntu_EC2"
+    Project = "Terraform_Lab"
+  }
 }
--- terraform/vpc-ec2/variables.tf
+++ terraform/vpc-ec2/variables.tf
@@ -1,15 +1,11 @@
-variable aws_profile {
-    description =   "The aws profile used to create the stack"
-}
 variable aws_region {
-    description =   "The region that the stack will be created in"
+  description = "The region that the stack will be created in"
 }
 
-
-variable vpc_cidr   {
-    description =   "The cidr for the vpc"
+variable vpc_cidr {
+  description = "The cidr for the vpc"
 }
 
 variable subnet_count {
-    description =   "How many subnets (of each public and private) to be created"
+  description = "How many subnets (of each public and private) to be created"
 }
